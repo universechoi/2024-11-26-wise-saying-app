e06a0df6776f75c4f06a8f3b6a93c5b9c9da5b73
universechoi <skye.of.universe@gmail.com>
2024-11-26 18:47:22 +0900

    test: runAppTest

---
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -0,0 +1,30 @@
+package com.ll.domain.wiseSaying.controller;
+
+import com.ll.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WiseSayingControllerTest {
+    @Test
+    @DisplayName("== 명언 앱 ==")
+    public void runAppTest() throws IOException {
+        Scanner scanner = TestUtil.getScanner("종료");
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output).contains("== 명언 앱 ==");
+    }
+}
