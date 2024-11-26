9dfd53a8677b83a8211ec5b9508eb0eed1c566a8
universechoi <skye.of.universe@gmail.com>
2024-11-26 18:27:54 +0900

    feat: setOutToByteArray

--- src/test/java/com/ll/standard/util/TestUtil.java
+++ src/test/java/com/ll/standard/util/TestUtil.java
@@ -1,9 +1,22 @@
 package com.ll.standard.util;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
     public static Scanner getScanner(String input) {
         return (new Scanner(input.stripIndent().trim()));
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream byteArrayOutputStream) throws IOException {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        byteArrayOutputStream.close();
     }
 }
--- src/test/java/com/ll/standard/util/TestUtilTest.java
+++ src/test/java/com/ll/standard/util/TestUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,8 +29,8 @@ public class TestUtilTest {
     }
 
     @Test
-    @DisplayName("TestUtil.setOutToByteArray()");
-    public void setOutToByteArrayTest() {
+    @DisplayName("TestUtil.setOutToByteArray()")
+    public void setOutToByteArrayTest() throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
         System.out.println("2 / 이순신 / 나의 죽음을 적에게 알리지 마라");
