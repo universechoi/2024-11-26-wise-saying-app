76a7418186db84f192622666c31ef31747d5b12e
universechoi <skye.of.universe@gmail.com>
2024-11-26 18:05:27 +0900

    test: setOutToByteArrayTest

--- src/test/java/com/ll/standard/util/TestUtilTest.java
+++ src/test/java/com/ll/standard/util/TestUtilTest.java
@@ -3,6 +3,7 @@ package com.ll.standard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,5 +25,19 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("나의 죽음을 적에게 알리지 마라");
         assertThat(author).isEqualTo("이순신");
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()");
+    public void setOutToByteArrayTest() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("2 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+
+        String out = byteArrayOutputStream.toString().trim();
+
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        assertThat(out).isEqualTo("2 / 이순신 / 나의 죽음을 적에게 알리지 마라");
     }
 }
