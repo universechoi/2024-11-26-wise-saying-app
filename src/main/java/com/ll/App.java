33d075d7c89eefa36c56147f23f6bd1f1e5dc378
universechoi <skye.of.universe@gmail.com>
2024-11-26 20:22:02 +0900

    feat: exit

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -11,5 +11,14 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.println("명령) ");
+
+            String cmd = scanner.nextLine();
+            if(cmd.equals("종료")) {
+                break;
+            }
+        }
     }
 }
