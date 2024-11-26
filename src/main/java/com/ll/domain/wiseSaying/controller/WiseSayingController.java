e8fb7966b3613b715df1c196da2c504ddc83cdbd
universechoi <skye.of.universe@gmail.com>
2024-11-26 22:21:23 +0900

    feat: wiseSaying

--- src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
@@ -1,19 +1,31 @@
 package com.ll.domain.wiseSaying.controller;
 
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingController {
+    private List<WiseSaying> wiseSayingList;
     private int lastId;
 
-    WiseSayingController() {
+    public WiseSayingController() {
         this.lastId = 0;
+        this.wiseSayingList = new ArrayList<>();
     }
 
     public void register(Scanner scanner) {
         System.out.println("명언 : ");
+        String content = scanner.nextLine();
+
         System.out.println("작가 : ");
+        String author = scanner.nextLine();
 
         int id = ++lastId;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayingList.add(wiseSaying);
 
         System.out.println(id + "번 명언이 등록되었습니다.");
     }
---
+++ src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
@@ -0,0 +1,33 @@
+package com.ll.domain.wiseSaying.entity;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
