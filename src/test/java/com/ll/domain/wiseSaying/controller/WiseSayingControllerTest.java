a9b3863c5e95b850ab064b6a1ff5fdfc193f4ac7
universechoi <skye.of.universe@gmail.com>
2024-11-26 20:20:04 +0900

    test: cmdTest2

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -30,4 +30,17 @@ public class WiseSayingControllerTest {
         assertThat(output)
                 .contains("명령) ");
     }
+
+    @Test
+    @DisplayName("명령을 2번 이상 내릴 수 있습니다.")
+    public void cmdTest2() throws IOException {
+        String output = AppTest.run("""
+                목록
+                목록
+                종료
+                """);
+
+        String[] split = output.split("명령\\) ");
+        assertThat(split).hasSize(4);
+    }
 }
