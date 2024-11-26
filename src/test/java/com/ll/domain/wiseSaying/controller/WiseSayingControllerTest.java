7c24323fa486baf055f184c3f6e150e8f2755bac
universechoi <skye.of.universe@gmail.com>
2024-11-26 20:49:36 +0900

    test: registerTest2

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -22,4 +22,21 @@ public class WiseSayingControllerTest {
                 .contains("명언 : ")
                 .contains("작가 : ");
     }
+
+    @Test
+    @DisplayName("등록할 때마다 명언 번호가 1씩 증가하여 출력된다.")
+    public void registerTest2() throws IOException {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라
+                작자미상
+                등록
+                과거에 집착하지 마라
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.");
+    }
 }
