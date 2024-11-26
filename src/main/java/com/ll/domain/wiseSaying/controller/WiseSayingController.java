d513bf92255f0fdebc7900bb104c78be52ea5426
universechoi <skye.of.universe@gmail.com>
2024-11-26 20:52:45 +0900

    feat: add id in register

--- src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
@@ -3,8 +3,18 @@ package com.ll.domain.wiseSaying.controller;
 import java.util.Scanner;
 
 public class WiseSayingController {
+    private int lastId;
+
+    WiseSayingController() {
+        this.lastId = 0;
+    }
+
     public void register(Scanner scanner) {
         System.out.println("명언 : ");
         System.out.println("작가 : ");
+
+        int id = ++lastId;
+
+        System.out.println(id + "번 명언이 등록되었습니다.");
     }
 }
