da1be532d4f7af7f2d39a4e3dbec07732de95944
universechoi <skye.of.universe@gmail.com>
2024-11-27 21:09:23 +0900

    refactor: commandTests

--- src/test/java/com/ll/CommandTest.java
+++ src/test/java/com/ll/CommandTest.java
@@ -7,42 +7,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommandTest {
     @Test
-    @DisplayName("Command")
+    @DisplayName("getActionName()")
     public void commandTest() {
         Command cmd = new Command("삭제?id=10");
         assertThat(cmd.getActionName()).isEqualTo("삭제");
     }
 
     @Test
-    @DisplayName("Command")
+    @DisplayName("getParam(\"id\") == 10")
     public void commandTest2() {
         Command cmd = new Command("삭제?id=10");
         assertThat(cmd.getParam("id")).isEqualTo("10");
     }
 
     @Test
-    @DisplayName("Command")
+    @DisplayName("getParam(\"number\") == null")
     public void commandTest3() {
         Command cmd = new Command("삭제?id=10");
         assertThat(cmd.getParam("number")).isNull();
     }
 
     @Test
-    @DisplayName("Command")
+    @DisplayName("getParam(\"number\") == -")
     public void commandTest4() {
         Command cmd = new Command("삭제?id=10");
-        assertThat(cmd.getParam("id", "-")).isEqualTo("-");
+        assertThat(cmd.getParam("number", "-")).isEqualTo("-");
     }
 
     @Test
-    @DisplayName("Command")
+    @DisplayName("getParamAsInt(\"id\") == 10")
     public void commandTest5() {
         Command cmd = new Command("삭제?id=10");
         assertThat(cmd.getParamAsInt("id", 0)).isEqualTo(10);
     }
 
     @Test
-    @DisplayName("Command")
+    @DisplayName("getParamAsInt(\"number\") == 0")
     public void commandTest6() {
         Command cmd = new Command("삭제?id=10");
         assertThat(cmd.getParamAsInt("number", 0)).isEqualTo(0);
