d994c5f60c7116e3d6a96ea39abfb1dee45da603
universechoi <skye.of.universe@gmail.com>
2024-11-27 21:47:07 +0900

    test: modifyTest2

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -126,4 +126,25 @@ public class WiseSayingControllerTest {
                 .contains("작가(기존) : 작자미상")
                 .contains("명언(기존) : 과거에 집착하지 마라.");
     }
+    @Test
+    @DisplayName("수정")
+    public void modifyTest2() throws IOException {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                수정?id=2
+                현재와 자신을 사랑하라.
+                홍길동
+                목록
+                """);
+
+        System.out.println(output);
+
+        assertThat(output)
+                .contains("2 / 홍길동 / 현재와 자신을 사랑하라.");
+    }
 }
