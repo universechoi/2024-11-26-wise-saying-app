7bc2973b09b19d87910cc6089c3df7dcf97e843a
universechoi <skye.of.universe@gmail.com>
2024-11-27 20:44:00 +0900

    feat: getParam

--- src/main/java/com/ll/Command.java
+++ src/main/java/com/ll/Command.java
@@ -1,16 +1,33 @@
 package com.ll;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Command {
     private final String actionName;
     private final String queryString;
+    private final Map<String, String> params;
 
     public Command(String cmd) {
         String[] split = cmd.trim().split("\\?");
         this.actionName = split[0];
+
+        this.params = new HashMap<>();
         this.queryString = split.length > 1 ? split[1] : "";
+
+        String[] queryStringBits = queryString.split("&");
+
+        for (String queryStringBit : queryStringBits) {
+            String[] keyValue = queryStringBit.split("=", 2);
+            this.params.put(keyValue[0], keyValue[1]);
+        }
     }
 
     public String getActionName() {
         return actionName;
+    }
+
+    public String getParam(String key) {
+        return this.params.get(key);
     }
 }
