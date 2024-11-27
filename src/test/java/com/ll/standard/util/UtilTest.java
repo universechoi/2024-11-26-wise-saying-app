faa43119a78f8c7c8187ae07084e1aa26f08916a
universechoi <skye.of.universe@gmail.com>
2024-11-28 00:33:55 +0900

    test: add beforeAll, afterAll

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -1,15 +1,27 @@
 package com.ll.standard.util;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilTest {
+    @BeforeAll
+    public static void beforeAll() {
+        Util.file.mkdir("temp");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        Util.file.rmdir("temp");
+    }
+
     @Test
     @DisplayName("파일을 생성할 수 있다.")
     public void createFileTest() {
-        String filePath = "test.txt";
+        String filePath = "temp/test.txt";
 
         Util.file.touch(filePath);
 
@@ -19,7 +31,7 @@ public class UtilTest {
     @Test
     @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
     public void editFileTest() {
-        String filePath = "test.txt";
+        String filePath = "temp/test.txt";
 
         Util.file.set(filePath, "내용");
 
@@ -30,7 +42,7 @@ public class UtilTest {
     @DisplayName("파일을 삭제할 수 있다.")
     public void deleteFileTest() {
         // given
-        String filePath = "test.txt";
+        String filePath = "temp/test.txt";
         // when
         Util.file.touch(filePath);
         Util.file.delete(filePath);
