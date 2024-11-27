9cb5ea875f38bed09e18429e8feaabd722d9c907
universechoi <skye.of.universe@gmail.com>
2024-11-28 00:26:24 +0900

    test: editFileTest

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -15,4 +15,14 @@ public class UtilTest {
 
         assertThat(Util.file.exists(filePath)).isTrue();
     }
+
+    @Test
+    @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
+    public void editFileTest() {
+        String filePath = "test.txt";
+
+        Util.file.set(filePath, "내용");
+
+        assertThat(Util.file.get(filePath, "")).isEqualTo("내용");
+    }
 }
