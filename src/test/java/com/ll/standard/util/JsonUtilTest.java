009f087ca470fda0a25c0e37c6ee92f7644796da
universechoi <skye.of.universe@gmail.com>
2024-11-28 21:58:37 +0900

    test: mapToJsonTest

---
+++ src/test/java/com/ll/standard/util/JsonUtilTest.java
@@ -0,0 +1,26 @@
+package com.ll.standard.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JsonUtilTest {
+    @Test
+    @DisplayName("Map을 Json으로 바꿀 수 있다.")
+    public void mapToJsonTest() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "이름");
+
+        String jsonStr = Util.json.toString(map);
+
+        assertThat(jsonStr).isEqualTo("""
+                {
+                    "name": "이름"
+                }
+                """.stripIndent().trim());
+    }
+}
