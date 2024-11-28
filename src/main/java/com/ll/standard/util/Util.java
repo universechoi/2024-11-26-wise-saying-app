2b38c68ee418b89a98500d8263bcbc9e11ffbb08
universechoi <skye.of.universe@gmail.com>
2024-11-28 22:01:14 +0900

    feat: toString

--- src/main/java/com/ll/standard/util/Util.java
+++ src/main/java/com/ll/standard/util/Util.java
@@ -3,6 +3,7 @@ package com.ll.standard.util;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Map;
 
 public class Util {
     public static class file {
@@ -94,6 +95,28 @@ public class Util {
         public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
             Files.delete(dir);
             return FileVisitResult.CONTINUE;
+        }
+    }
+
+    public static class json {
+        private json() {
+
+        }
+
+        public static String toString(Map<String, Object> map) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("{");
+            sb.append("\n");
+
+            map.forEach((String key, Object value)->{
+                sb.append("    ");
+                sb.append("\"%s\": \"%s\"".formatted(key, value));
+            });
+
+            sb.append("\n");
+            sb.append("}");
+            return sb.toString();
         }
     }
 }
