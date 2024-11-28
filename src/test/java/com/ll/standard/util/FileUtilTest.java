4bb26568b742593aae7dc86f6d6addcbfece7462
universechoi <skye.of.universe@gmail.com>
2024-11-28 21:31:59 +0900

    refactor: rename

--- src/test/java/com/ll/standard/util/FileUtilTest.java
+++ src/test/java/com/ll/standard/util/FileUtilTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UtilTest {
+public class FileUtilTest {
     @BeforeAll
     public static void beforeAll() {
         Util.file.mkdir("temp");
