f42d72581eedc0a2dba38b3b9d78258a41f24c0a
universechoi <skye.of.universe@gmail.com>
2024-11-28 21:26:11 +0900

    refactor: add lombok

--- src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
@@ -1,37 +1,16 @@
 package com.ll.domain.wiseSaying.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
-
-    public WiseSaying(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setId(int id) { this.id = id; }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 
     public boolean isNew() {
         return id == 0;
