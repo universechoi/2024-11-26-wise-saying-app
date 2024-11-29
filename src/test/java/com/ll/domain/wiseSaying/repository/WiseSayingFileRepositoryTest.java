db45d9fd847da58a02ccea93cb289b610949c23e
universechoi <skye.of.universe@gmail.com>
2024-11-29 11:18:57 +0900

    test: saveTest

---
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.ll.domain.wiseSaying.repository;
+
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+import com.ll.standard.util.Util;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WiseSayingFileRepositoryTest {
+    private final WiseSayingRepository wiseSayingRepository= new WiseSayingFileRepository();
+
+    @BeforeAll
+    public static void beforeAll() {
+        Util.file.rmdir("db");
+        Util.file.mkdir("db");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        Util.file.rmdir("db");
+    }
+
+    @Test
+    @DisplayName("명언 저장")
+    public void saveTest() {
+        wiseSayingRepository.save(new WiseSaying(0, "명언1", "명언2"));
+
+        String filePath =  "db/test/wiseSaying/1.json";
+
+        assertThat(Util.file.exists(filePath)).isTrue();
+    }
+}
